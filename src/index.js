f29b444f5dbf684e63f6f69fd2f778d10b0d668a
Alexey <belshina25@gmail.com>
2020-10-17 17:17:21 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let len = bracketsConfig.length;
+
+  for (let i = 0; i < len;) {
+
+    let pair = bracketsConfig[i].join('');
+
+    if (str.includes(pair)) {
+      str = str.replace(pair, '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+  return str.length == 0 ? true : false;
+  
 }
